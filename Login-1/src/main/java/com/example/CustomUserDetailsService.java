c292de38d812b27fc5089cf5c18e596c411fbde9
Paloma <yppr09@gmail.com>
2021-02-03 16:42:47 -0600

    login and CRUD

---
+++ Login-1/src/main/java/com/example/CustomUserDetailsService.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+ 
+
+public class CustomUserDetailsService implements UserDetailsService, UserDetails{
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	 public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	        User user = userRepo.findByEmail(username);	        
+	        if (user == null) {
+	            throw new UsernameNotFoundException("User not found");
+	        }
+	        return new CustomUserDetails(user);
+	    }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ Login-1/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+import com.example.commons.GenericService;
+import com.example.User;
+
+public interface UserService extends GenericService<User, Long>{
+
+}
